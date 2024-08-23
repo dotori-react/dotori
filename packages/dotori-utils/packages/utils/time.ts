282e1258b70832aed7537ff98763b841e148c4f7
LEE DONG GYU <tenenger7125@gmail.com>
2024-08-23 20:29:08 +0900

    [dotori-utils@feat]: add time utils

--- packages/dotori-utils/packages/utils/time.ts
+++ packages/dotori-utils/packages/utils/time.ts
@@ -15,3 +15,17 @@ export const measureTime = async (callback: () => void | Promise<void>) => {
 
   return endTime - startTime;
 };
+
+export const getTime = (_milliseconds: number) => ({
+  hours: Math.floor(_milliseconds / 1000 / 60 / 60) % 60,
+  minutes: Math.floor(_milliseconds / 1000 / 60) % 60,
+  seconds: Math.floor(_milliseconds / 1000) % 60,
+  milliseconds: _milliseconds % 1000,
+  raw: _milliseconds,
+});
+
+export const getDisplayTime = (_milliseconds: number) => {
+  const { hours, minutes, seconds, milliseconds } = getTime(_milliseconds);
+
+  return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}:${milliseconds.toString().padStart(3, '0')}`;
+};
