57c9cf6d0e0f3cb5aec50f45a6b3c1846dad4a6a
LEE DONG GYU <tenenger7125@gmail.com>
2024-09-11 20:53:24 +0900

    [dotori-components@style]: update Chip component size

--- packages/dotori-components/packages/components/chip/Chip.tsx
+++ packages/dotori-components/packages/components/chip/Chip.tsx
@@ -32,8 +32,10 @@ const Chip = ({ size, color, defaultChecked = false, variant, value = '', childr
         checked={ctx ? contextProps.checked : checked}
         name="test"
       />
-      {(ctx ? contextProps.checked : checked) && <Icon className={iconStyle({ size, color, variant })} icon="check" />}
-      <span className="">{children}</span>
+      {(ctx ? contextProps.checked : checked) && (
+        <Icon className={iconStyle({ color, variant })} icon="check" fullSize />
+      )}
+      <span className="text-inherit whitespace-nowrap">{children}</span>
     </label>
   );
 };
@@ -49,68 +51,54 @@ const defaultProps = {
   checked: false,
 };
 
-const chipContainerStyle = cn('inline-flex cursor-pointer items-center justify-center gap-1 rounded-md px-2', {
-  variants: {
-    size: {
-      xs: 'h-4 text-3xs',
-      sm: 'h-5 text-2xs',
-      md: 'h-6 text-xs',
-      lg: 'h-7 text-sm',
-      xl: 'h-8 text-md',
+const chipContainerStyle = cn(
+  'inline-flex cursor-pointer items-center gap-1 rounded-md px-2 box-content align-middle overflow-hidden',
+  {
+    variants: {
+      size: {
+        xs: 'h-4 text-3xs',
+        sm: 'h-5 text-2xs',
+        md: 'h-6 text-xs',
+        lg: 'h-7 text-sm',
+        xl: 'h-8 text-md',
+      },
+      checked: {
+        true: 'border',
+        false: '',
+      },
+      color: {
+        black: 'bg-gray-900 text-gray-900 hover:bg-black border-gray-900',
+        blue: 'bg-blue-600 text-blue-600 hover:bg-blue-700 border-blue-600',
+        gray: 'bg-gray-600 text-gray-600 hover:bg-gray-700 border-gray-600',
+        green: 'bg-green-600 text-green-600 hover:bg-green-700 border-green-600',
+        red: 'bg-red-600 text-red-600 hover:bg-red-700 border-red-600',
+        yellow: 'bg-yellow-600 text-yellow-600 hover:bg-yellow-700 border-yellow-600',
+      },
+      variant: {
+        filled: 'text-gray-200',
+        outline: 'border bg-transparent hover:bg-opacity-10',
+        subtle: 'border-none bg-gray-200 hover:bg-gray-300',
+      },
     },
-    color: {
-      black: 'bg-gray-900 hover:bg-black',
-      blue: 'bg-blue-600 text-blue-600 hover:bg-blue-700',
-      gray: 'bg-gray-600 text-gray-600 hover:bg-gray-700',
-      green: 'bg-green-600 text-green-600 hover:bg-green-700',
-      red: 'bg-red-600 text-red-600 hover:bg-red-700',
-      yellow: 'bg-yellow-600 text-yellow-600 hover:bg-yellow-700',
-    },
-    variant: {
-      filled: 'text-gray-200',
-      outline: 'border bg-transparent hover:bg-opacity-10',
-      subtle: 'bg-gray-200 text-gray-900 hover:bg-gray-300',
-    },
-    checked: {
-      true: '',
-      false: '',
+    compoundVariants: [
+      { variant: 'subtle', checked: false, className: 'text-gray-900' },
+      { color: 'black', variant: 'subtle', checked: true, className: 'bg-gray-100 hover:bg-gray-200' },
+      { color: 'blue', variant: 'subtle', checked: true, className: 'bg-blue-100 hover:bg-blue-200' },
+      { color: 'gray', variant: 'subtle', checked: true, className: 'bg-gray-100 hover:bg-gray-200' },
+      { color: 'green', variant: 'subtle', checked: true, className: 'bg-green-100 hover:bg-green-200' },
+      { color: 'red', variant: 'subtle', checked: true, className: 'bg-red-100 hover:bg-red-200' },
+      { color: 'yellow', variant: 'subtle', checked: true, className: 'bg-yellow-100 hover:bg-yellow-200' },
+    ],
+    defaultVariants: {
+      size: 'sm',
+      color: 'black',
+      variant: 'outline',
     },
   },
-  compoundVariants: [
-    { color: 'black', variant: 'outline', checked: true, className: 'border-gray-900' },
-    { color: 'blue', variant: 'outline', checked: true, className: 'border-blue-600' },
-    { color: 'gray', variant: 'outline', checked: true, className: 'border-gray-600' },
-    { color: 'green', variant: 'outline', checked: true, className: 'border-green-600' },
-    { color: 'red', variant: 'outline', checked: true, className: 'border-red-600' },
-    { color: 'yellow', variant: 'outline', checked: true, className: 'border-yellow-600' },
-    { color: 'black', variant: 'subtle', checked: true, className: 'bg-gray-100 text-gray-900 hover:bg-gray-200' },
-    { color: 'blue', variant: 'subtle', checked: true, className: 'bg-blue-100 text-blue-600 hover:bg-blue-200' },
-    { color: 'gray', variant: 'subtle', checked: true, className: 'bg-gray-100 text-gray-600 hover:bg-gray-200' },
-    { color: 'green', variant: 'subtle', checked: true, className: 'bg-green-100 text-green-600 hover:bg-green-200' },
-    { color: 'red', variant: 'subtle', checked: true, className: 'bg-red-100 text-red-600 hover:bg-red-200' },
-    {
-      color: 'yellow',
-      variant: 'subtle',
-      checked: true,
-      className: 'bg-yellow-100 text-yellow-600 hover:bg-yellow-200',
-    },
-  ],
-  defaultVariants: {
-    size: 'sm',
-    color: 'black',
-    variant: 'outline',
-  },
-});
+);
 
 const iconStyle = cn('', {
   variants: {
-    size: {
-      xs: 'h-4 w-4',
-      sm: 'h-5 w-5',
-      md: 'h-6 w-6',
-      lg: 'h-7 w-7',
-      xl: 'h-8 w-8',
-    },
     color: {
       black: 'fill-gray-900',
       blue: 'fill-blue-600',
@@ -126,7 +114,6 @@ const iconStyle = cn('', {
     },
   },
   defaultVariants: {
-    size: 'sm',
     color: 'black',
     variant: 'outline',
   },
