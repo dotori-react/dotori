720c05bc46fcb8d6b0101ff18b5f70dfd5a660e5
LEE DONG GYU <tenenger7125@gmail.com>
2024-09-22 21:25:58 +0900

    [dotori-components@config]: add Button storybook

---
+++ stories/components/Button.stories.tsx
@@ -0,0 +1,82 @@
+import { Button } from 'dotori-components';
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+  title: 'buttons/Button',
+  component: Button,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    children: {
+      control: 'text',
+      description: 'button text',
+    },
+    size: {
+      control: 'select',
+      description: 'button size',
+      options: ['xs', 'sm', 'md', 'lg', 'xl'],
+    },
+    variant: {
+      control: 'select',
+      description: 'button category',
+      options: ['filled', 'outline', 'subtle', 'unstyle'],
+    },
+    color: {
+      control: 'select',
+      description: 'button color',
+      options: ['black', 'blue', 'gray', 'green', 'red', 'yellow'],
+    },
+    disabled: {
+      control: 'boolean',
+      description: 'button disabled presence or absence',
+    },
+    fullWidth: {
+      control: 'boolean',
+      description: 'button full size presence or absence',
+    },
+  },
+  args: {
+    children: 'button',
+    size: 'xs',
+    variant: 'filled',
+    color: 'black',
+    disabled: false,
+    fullWidth: false,
+  },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    variant: 'filled',
+  },
+};
+
+export const Filled: Story = {
+  args: {
+    variant: 'filled',
+  },
+};
+
+export const Outline: Story = {
+  args: {
+    variant: 'outline',
+  },
+};
+
+export const Subtle: Story = {
+  args: {
+    variant: 'subtle',
+  },
+};
+
+export const Unstyle: Story = {
+  args: {
+    variant: 'unstyle',
+  },
+};
