dcf14640cd6d6243a20b08ce9ec1a31f0ff024cb
LEE DONG GYU <tenenger7125@gmail.com>
2024-09-27 19:07:55 +0900

    [dotori-components@style]: update PillInput container style

--- packages/dotori-components/packages/components/pillInput/PillInput.tsx
+++ packages/dotori-components/packages/components/pillInput/PillInput.tsx
@@ -1,10 +1,12 @@
 import { useState } from 'react';
 
+import { cn } from 'dotori-utils';
+
 import { Input, Pill } from '@dotori-components/components';
 
 import type { InputProps } from '@dotori-components/components/input/Input';
 
-const PillInput = ({ value, onChange, size, ...inputProps }: PillInputProps) => {
+const PillInput = ({ value, onChange, size, disabled, ...inputProps }: PillInputProps) => {
   const [pillValues, setPillValues] = useState<string[]>(value ?? []);
 
   const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
@@ -37,13 +39,13 @@ const PillInput = ({ value, onChange, size, ...inputProps }: PillInputProps) => 
   };
 
   return (
-    <div className="flex flex-wrap items-center gap-1 border border-gray-200 px-3 py-1">
+    <div className={pillInputContainerStyle({ disabled })}>
       {pillValues.map((v, i) => (
         <Pill key={i} size={size} withCloseButton onClose={() => handlePillDelete(i)}>
           {v}
         </Pill>
       ))}
-      <Input {...inputProps} className="h-full min-w-24 flex-1 border-none p-0" size={size} onKeyDown={onKeyDown} />
+      <Input {...inputProps} className="h-full min-w-full flex-1 border-none" size={size} onKeyDown={onKeyDown} />
     </div>
   );
 };
@@ -52,5 +54,14 @@ interface PillInputProps extends Omit<InputProps, 'value' | 'onChange'> {
   value: string[];
   onChange: (values: string[]) => void;
 }
+
+const pillInputContainerStyle = cn('flex flex-wrap items-center gap-1 border border-gray-200 px-3 py-1', {
+  variants: {
+    disabled: {
+      true: 'cursor-not-allowed bg-gray-100',
+      false: '',
+    },
+  },
+});
 
 export default PillInput;
