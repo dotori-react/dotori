00d271a89d86ea399cbecf86948ebfc55c9d541f
LEE DONG GYU <tenenger7125@gmail.com>
2024-09-27 19:00:40 +0900

    [dotori-components@style]: update Input size style

--- packages/dotori-components/packages/components/input/Input.tsx
+++ packages/dotori-components/packages/components/input/Input.tsx
@@ -27,7 +27,7 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
     }, [combinedFocused]);
 
     return (
-      <label className={inputContainerStyle({ className, disabled, size, focused: combinedFocused })}>
+      <label className={inputContainerStyle({ className, disabled, focused: combinedFocused })}>
         {leftIcon && <span className={iconStyle({ size })}>{leftIcon}</span>}
         <input
           ref={node => {
@@ -35,7 +35,7 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
             if (ref && typeof ref !== 'function') ref.current = node;
             inputRef.current = node;
           }}
-          className={inputStyle({ disabled })}
+          className={inputStyle({ disabled, size })}
           disabled={disabled}
           placeholder={placeholder}
           type={type}
@@ -69,40 +69,41 @@ const inputContainerStyle = cn('flex cursor-text items-center gap-1 rounded bord
       true: 'cursor-not-allowed bg-gray-100',
       false: '',
     },
-    size: {
-      xs: 'h-10 text-xs',
-      sm: 'h-12 text-sm',
-      md: 'h-14 text-md',
-      lg: 'h-16 text-lg',
-      xl: 'h-18 text-xl',
-    },
     type: {
       file: 'cursor-pointer text-gray-400',
       text: '',
     },
   },
-  defaultVariants: {
-    size: 'md',
-  },
 });
 
 const inputStyle = cn('w-full px-3 placeholder:text-gray-400', {
   variants: {
+    size: {
+      xs: 'h-4 text-xs',
+      sm: 'h-6 text-sm',
+      md: 'h-7 text-md',
+      lg: 'h-8 text-lg',
+      xl: 'h-10 text-xl',
+    },
     disabled: {
       true: 'cursor-not-allowed bg-gray-100',
       false: '',
     },
+  },
+  defaultVariants: {
+    size: 'md',
+    disabled: false,
   },
 });
 
 const iconStyle = cn('inline-flex items-center justify-center', {
   variants: {
     size: {
-      xs: 'h-10 w-10',
-      sm: 'h-12 w-12',
-      md: 'h-14 w-14',
-      lg: 'h-16 w-16',
-      xl: 'h-18 w-18',
+      xs: 'h-4',
+      sm: 'h-6',
+      md: 'h-7',
+      lg: 'h-8',
+      xl: 'h-10',
     },
   },
   defaultVariants: {
