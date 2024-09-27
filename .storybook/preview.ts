f713b9d648607c8dd93312404469ae223ae71c63
LEE DONG GYU <tenenger7125@gmail.com>
2024-09-27 10:16:01 +0900

    [dotori-repo@config]: add storybook backgrounds

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,6 +2,16 @@ import type { Preview } from '@storybook/react';
 
 const preview: Preview = {
   parameters: {
+    backgrounds: {
+      values: [
+        // 👇 Default values
+        { name: 'Dark', value: '#333' },
+        { name: 'Light', value: '#FFF' },
+        // 👇 Add your own
+      ],
+      // 👇 Specify which background is shown by default
+      // default: 'Light',
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
