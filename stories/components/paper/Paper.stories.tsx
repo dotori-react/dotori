e5c9b19b581ce79fae5c83531664f3443c217efd
LEE DONG GYU <tenenger7125@gmail.com>
2024-09-27 10:16:25 +0900

    [dotori-components@config]: add Paper storybook

---
+++ stories/components/paper/Paper.stories.tsx
@@ -0,0 +1,62 @@
+import { Paper } from 'dotori-components';
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+  title: 'layout/Paper',
+  component: Paper,
+  tags: ['autodocs'],
+  parameters: {
+    backgrounds: {
+      // 👇 Set default background value for all component stories
+      default: 'Dark',
+    },
+  },
+  decorators: Story => (
+    <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+      <Story />
+    </div>
+  ),
+  argTypes: {
+    size: {
+      control: 'select',
+      description: "*(Optional)* Determines the width of the Paper component. One of 'xs' | 'sm' | 'md' | 'lg' | 'xl'.",
+      options: ['xs', 'sm', 'md', 'lg', 'xl'],
+      table: {
+        defaultValue: { summary: 'md' },
+      },
+    },
+    fullWidth: {
+      control: 'boolean',
+      description:
+        '*(Optional)* A boolean that, if true, makes the Paper component occupy the full width of its container.',
+      table: {
+        defaultValue: { summary: 'false' },
+      },
+    },
+    className: {
+      control: 'text',
+      description: "*(Optional)* Additional class names to customize the Paper component's appearance.",
+      table: {
+        defaultValue: { summary: '' },
+      },
+    },
+    children: {
+      actions: 'changed',
+      description: '*(Optional)* The content to be displayed inside the Paper component.',
+    },
+  },
+  args: {
+    size: 'md',
+    fullWidth: false,
+    className: '',
+    children: 'paper contents',
+  },
+} satisfies Meta<typeof Paper>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Example: Story = {
+  args: {},
+};
