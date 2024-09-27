49a19c262aa7ba8c7c9bfb9d01d6c277f34c8e29
LEE DONG GYU <tenenger7125@gmail.com>
2024-09-27 18:40:44 +0900

    [dotori-components@fix]: add pill size, required value and onChange function

--- packages/dotori-components/packages/components/pillInput/PillInput.tsx
+++ packages/dotori-components/packages/components/pillInput/PillInput.tsx
@@ -4,7 +4,7 @@ import { Input, Pill } from '@dotori-components/components';
 
 import type { InputProps } from '@dotori-components/components/input/Input';
 
-const PillInput = ({ value, onChange, ...omittedInputProps }: PillInputProps) => {
+const PillInput = ({ value, onChange, size, ...inputProps }: PillInputProps) => {
   const [pillValues, setPillValues] = useState<string[]>(value ?? []);
 
   const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
@@ -39,18 +39,18 @@ const PillInput = ({ value, onChange, ...omittedInputProps }: PillInputProps) =>
   return (
     <div className="flex flex-wrap items-center gap-1 border border-gray-200 px-3 py-1">
       {pillValues.map((v, i) => (
-        <Pill key={i} withCloseButton onClose={() => handlePillDelete(i)}>
+        <Pill key={i} size={size} withCloseButton onClose={() => handlePillDelete(i)}>
           {v}
         </Pill>
       ))}
-      <Input {...omittedInputProps} className="h-full min-w-24 flex-1 border-none p-0" onKeyDown={onKeyDown} />
+      <Input {...inputProps} className="h-full min-w-24 flex-1 border-none p-0" size={size} onKeyDown={onKeyDown} />
     </div>
   );
 };
 
 interface PillInputProps extends Omit<InputProps, 'value' | 'onChange'> {
-  value?: string[];
-  onChange?: (values: string[]) => void;
+  value: string[];
+  onChange: (values: string[]) => void;
 }
 
 export default PillInput;
