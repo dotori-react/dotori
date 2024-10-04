e1bf25180086f2a1c884402d323520361cc54825
LEE DONG GYU <tenenger7125@gmail.com>
2024-10-04 23:34:49 +0900

    [dotori-components@feat]: add switch on, off callback props

--- packages/dotori-components/packages/components/switch/DarkModeSwitch.tsx
+++ packages/dotori-components/packages/components/switch/DarkModeSwitch.tsx
@@ -3,12 +3,21 @@ import { useDarkMode } from 'dotori-hooks';
 import { DarkModeSwitchProvider } from './DarkModeSwitch.context';
 import Switch, { type SwitchProps } from './Switch';
 
-const DarkModeSwitch = (props: Omit<SwitchProps, 'checked' | 'off' | 'on' | 'children'>) => {
-  const { isDark, on, off } = useDarkMode();
+const DarkModeSwitch = ({ on, off, ...rest }: Partial<Omit<SwitchProps, 'checked' | 'children'>>) => {
+  const { isDark, on: darkOn, off: darkOff } = useDarkMode();
+
+  const switchOn = () => {
+    darkOn();
+    if (on) on();
+  };
+  const switchOff = () => {
+    darkOff();
+    if (off) off();
+  };
 
   return (
     <DarkModeSwitchProvider value={{ isDarkModeSwitch: true }}>
-      <Switch {...props} checked={isDark} off={off} on={on} />
+      <Switch {...rest} checked={isDark} off={switchOff} on={switchOn} />
     </DarkModeSwitchProvider>
   );
 };
