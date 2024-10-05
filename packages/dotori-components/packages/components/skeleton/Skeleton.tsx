2ce07a3648085826e09b5a68a631e5c8905b4740
LEE DONG GYU <tenenger7125@gmail.com>
2024-10-05 21:38:15 +0900

    [dotori-components@feat]: update to toggle content visibility based on isSkeletonShow

--- packages/dotori-components/packages/components/skeleton/Skeleton.tsx
+++ packages/dotori-components/packages/components/skeleton/Skeleton.tsx
@@ -1,25 +1,40 @@
 import { cn, VariantProps } from 'dotori-utils';
 
-const Skeleton = ({ circle, className }: SkeletonProps) => <div className={skeletonStyle({ className, circle })} />;
+const Skeleton = ({ isSkeletonShow, isBorderRound, className, children, ...rest }: SkeletonProps) => (
+  <div className={skeletonStyle({ isSkeletonShow, className, isBorderRound })} {...rest}>
+    <div className={skeletonInnerStyle({ isInnerShow: !isSkeletonShow })}>{children}</div>
+  </div>
+);
 
 interface SkeletonProps
   extends React.ComponentPropsWithoutRef<'section'>,
-    Omit<VariantProps<typeof skeletonStyle>, 'isShow'>,
-    Required<Pick<VariantProps<typeof skeletonStyle>, 'isShow'>> {}
+    Omit<VariantProps<typeof skeletonStyle>, 'isSkeletonShow'>,
+    Required<Pick<VariantProps<typeof skeletonStyle>, 'isSkeletonShow'>> {
+  children: React.ReactNode;
+}
 
-const skeletonStyle = cn('h-full w-full animate-pulse bg-gray-200', {
+const skeletonStyle = cn('h-full w-full bg-gray-200', {
   variants: {
-    circle: {
+    isBorderRound: {
       true: 'rounded-full',
       false: 'rounded-none',
     },
-    isShow: {
-      true: 'visible',
+    isSkeletonShow: {
+      true: 'visible animate-pulse',
       false: 'invisible',
     },
   },
   defaultVariants: {
-    circle: false,
+    isBorderRound: false,
+  },
+});
+
+const skeletonInnerStyle = cn('', {
+  variants: {
+    isInnerShow: {
+      true: 'visible',
+      false: 'invisible',
+    },
   },
 });
 
