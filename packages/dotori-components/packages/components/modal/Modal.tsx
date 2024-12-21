55af02844a3841d7861ab07c744e83c686e2fdbc
LEE DONG GYU <tenenger7125@gmail.com>
2024-12-21 23:07:25 +0900

    [dotori-components@fix]: close modal background clicked when doubly modal opened

--- packages/dotori-components/packages/components/modal/Modal.tsx
+++ packages/dotori-components/packages/components/modal/Modal.tsx
@@ -2,16 +2,21 @@ import { useCreateElement, useOutSideClick } from 'dotori-hooks';
 
 import { CloseButton, Paper, Portal, type PaperProps } from '@dotori-components/components';
 
-const Modal = ({ isOpen, close, children, ...paperProps }: ModalProps) => {
+const Modal = ({ isOpen, close, children, onMouseDown, ...paperProps }: ModalProps) => {
   const ref = useOutSideClick<HTMLDivElement>(close);
   useCreateElement([{ tagName: 'div', attributes: { id: 'modal' } }]);
+
+  const handleMouseDown: React.MouseEventHandler<HTMLDivElement> = e => {
+    e.stopPropagation();
+    if (onMouseDown) onMouseDown(e);
+  };
 
   return (
     <Portal target={document.getElementById('modal') as HTMLElement}>
       {isOpen && (
         <div className="tw-preflight">
           <section className="fixed left-0 top-0 z-background flex h-full w-full items-center justify-center bg-dimmed">
-            <Paper ref={ref} {...paperProps}>
+            <Paper ref={ref} {...paperProps} onMouseDown={handleMouseDown}>
               <div className="mb-[10px] flex flex-row-reverse">
                 <CloseButton onClick={close} />
               </div>
