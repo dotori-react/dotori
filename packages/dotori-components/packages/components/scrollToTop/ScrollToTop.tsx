acbb99f37ed3c8335f24ac5c94cc6007eb7a443b
LEE DONG GYU <tenenger7125@gmail.com>
2025-03-18 23:09:21 +0900

    [dotori-components@style]: add className prop into ScrollToTop component

--- packages/dotori-components/packages/components/scrollToTop/ScrollToTop.tsx
+++ packages/dotori-components/packages/components/scrollToTop/ScrollToTop.tsx
@@ -1,10 +1,11 @@
 import { useEffect } from 'react';
 
 import { useDebounce, useDisClosure } from 'dotori-hooks';
+import { cn } from 'dotori-utils';
 
 import { ActionIcon } from '@dotori-components/components';
 
-const ScrollToTop = ({ showedMinHeight = 0 }: ScrollToTopProps) => {
+const ScrollToTop = ({ showedMinHeight = 0, className }: ScrollToTopProps) => {
   const { isOpen, open, close } = useDisClosure();
   const { debounce } = useDebounce({
     callback: () => {
@@ -28,11 +29,7 @@ const ScrollToTop = ({ showedMinHeight = 0 }: ScrollToTopProps) => {
   return (
     <div className="tw-preflight">
       {isOpen && (
-        <ActionIcon
-          className="fixed w-10 h-10 p-1 text-gray-600 -rotate-90 border border-gray-200 rounded-full shadow-inner bottom-6 right-6 bg-gray-0 active:bg-gray-300"
-          icon="arrowRightAlt"
-          onClick={handleScrollToTopClick}
-        />
+        <ActionIcon className={scrollToTopStyle({ className })} icon="arrowRightAlt" onClick={handleScrollToTopClick} />
       )}
     </div>
   );
@@ -40,6 +37,11 @@ const ScrollToTop = ({ showedMinHeight = 0 }: ScrollToTopProps) => {
 
 interface ScrollToTopProps {
   showedMinHeight?: number;
+  className?: string;
 }
+
+const scrollToTopStyle = cn(
+  'fixed w-10 h-10 p-1 text-gray-600 -rotate-90 border border-gray-200 rounded-full shadow-inner bottom-6 right-6 bg-gray-0 active:bg-gray-300',
+);
 
 export default ScrollToTop;
